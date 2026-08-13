ca5fa0fb9025a07ec5541cc80448171db9d9f38c
sikenali <mrchiang1015@aliyun.com>
2026-08-13 11:44:58 +0800

    fix(ui): rebuild with correct ri-bookmark-2-line logo icon, fix vite config

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,16 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
+  },
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/main.js'),
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        cssEntryFileNames: 'assets/[name]-[hash].css',
+        cssChunkFileNames: 'assets/[name]-[hash].css',
+      }
+    }
   }
 })
