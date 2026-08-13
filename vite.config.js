7585c095b6c6a7ea585b5b94b379dd003240cac2
sikenali <mrchiang1015@aliyun.com>
2026-08-13 11:58:22 +0800

    chore: rebuild with correct vite config and bookmark logo

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,5 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  },
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/main.js'),
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        cssEntryFileNames: 'assets/[name]-[hash].css',
-        cssChunkFileNames: 'assets/[name]-[hash].css',
-      }
-    }
   }
 })
